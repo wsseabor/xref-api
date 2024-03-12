ea2f840fd16e6f4d0798dceedd986211685c11a9
Schuyler Seaborn <wsseabor@gmail.com>
2024-03-11 20:14:41 -0400

    Major refactor, wind in sails

---
+++ data/data.py
@@ -0,0 +1,33 @@
+"""
+Module to store data for field headers in writing CSV files
+"""
+
+player_last_five_fields = [
+    'Date', 
+    'Team', 
+    '', 
+    'Opp', 
+    'Result', 
+    'GS', 
+    'MP', 
+    'FG', 
+    'FGA', 
+    'FG%', 
+    '3P', 
+    '3PA', 
+    '3P%', 
+    'FT', 
+    'FTA', 
+    'FT%', 
+    'ORB', 
+    'DRB', 
+    'TRB', 
+    'AST', 
+    'STL', 
+    'BLK', 
+    'TOV', 
+    'PF', 
+    'PTS',
+    'GmSc', 
+    '+/-'
+    ]
