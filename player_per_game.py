cc3327f8a29e3ebcec6cbf8596b46d01db48f899
Schuyler Seaborn <wsseabor@gmail.com>
2024-03-15 18:18:01 -0400

    Added driver and driver options to as class variable

--- player_per_game.py
+++ player_per_game.py
@@ -7,9 +7,6 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 
-#Deprecated?
-#DRIVER = "/usr/local/bin/chromedriver"
-
 """
 Inherits from base player stat class
 """
@@ -17,13 +14,24 @@ class PlayerPerGameStats(BasePlayerStats):
     
     def __init__(self, player_name):
         self.player_name = player_name
+        self.options = Options()
+        self.options.add_argument("--headless=new")
+        self.options.add_experimental_option(
+            "prefs", {
+                "profile.managed_default_content_settings.images" : 2,
+            }
+        )
+        self.browser = webdriver.Chrome(options=self.options)
 
     """
     Performs similar to a runtime script when class is called
     """
     def __call__(self):
-        print("Scraping per game data...")
+        print("Scraping per game column data...")
         self.get_player_column_headers()
+
+        print("Scraping per game row data...")
+        self.get_player_row_stats()
 
         print("Scrape ok...")
 
@@ -39,30 +47,37 @@ class PlayerPerGameStats(BasePlayerStats):
 
         Experimental options - do not load images
         """
-        options = Options()
-        options.add_argument("--headless=new")
-        options.add_experimental_option(
-            "prefs", {
-                "profile.managed_default_content_settings.images" : 2,
-            }
-        )
 
-        browser = webdriver.Chrome(options=options)
         url = "https://www.basketball-reference.com/players/w/wembavi01.html"
-        browser.get(url)
+        self.browser.get(url)
 
         try:
-            table = browser.find_element(By.ID, 'per_game')
+            table = self.browser.find_element(By.ID, 'per_game')
             headers = table.find_elements(By.XPATH, './thead/tr')
             column_headers = [header.text for header in headers[0].find_elements(By.TAG_NAME, 'th')]
-            print(column_headers)
-            browser.quit()
-            
-        except TimeoutException:
-            pass
 
+        except TimeoutException:
+            self.browser.quit()
+
+        return column_headers
+    
     def get_player_row_stats(self) -> list:
-        pass
+
+        """
+        Selenium webdriver boilerplate
+        """
+
+        url = "https://www.basketball-reference.com/players/w/wembavi01.html"
+        self.browser.get(url)
+
+        try:
+            table = self.browser.find_element(By.ID, 'per_game')
+            rows = table.find_elements(By.XPATH, './tbody/tr/td')
+            player_data = [row.text for row in rows]
+
+        except TimeoutException:
+            self.browser.quit()
+        
 
     def parse_player_stats(self) -> list:
         pass
